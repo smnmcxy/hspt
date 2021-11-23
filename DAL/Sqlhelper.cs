9b9ffc02bb719710ce2f9a237b07ee9b04f5fdfa
smnmcxy <94232807+smnmcxy@users.noreply.github.com>
2021-11-23 22:27:29 +0800

    添加项目文件。

---
+++ DAL/Sqlhelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    
+    public class Sqlhelper
+    {
+        private static string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+
+    }
+}
